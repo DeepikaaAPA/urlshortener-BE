09905fa4872a449f5d00206f9337bbabbe38e55c
DeepikaaAPA <deepikaarjun@gmail.com>
2024-09-03 14:41:40 +0530

    cors added

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const userRouter = require("./routes/userRouter");
 const cors = require("cors");
+const corsOptions = {
+  origin: "https://password-reset-flow-react-app.netlify.app",
+  credentials: true,
+};
 
 // create a new express app
 const app = express();
 
 // use the cors middleware
-app.use(cors());
+app.use(cors(corsOptions));
 // use the express json middleware for parsing json data
 app.use(express.json());
 
